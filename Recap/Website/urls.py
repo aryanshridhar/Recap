4243a4cabdda6cf2f10638ffdf6f6d41ce863933
aryanshridhar <aryanshridhar7@gmail.com>
2020-03-12 13:35:48 +0530

    Incs hall

--- Recap/Website/urls.py
+++ Recap/Website/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
+
 urlpatterns = [
     path('' , views.homepage , name = 'homepage'),
     path('results/<str:url>' , views.results , name = 'results'),
--- Recap/Website/views.py
+++ Recap/Website/views.py
@@ -1,13 +1,52 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from youtube_transcript_api import YouTubeTranscriptApi
+from gensim.summarization import summarize
+import re
+
+def summarize1():
+    f = open('file.txt','r')
+    raw = f.read()
+    f.close()
+    sumz=summarize(raw,ratio = .3)
+
+    return sumz
+
+def id_video(url):
+    result = re.match('^[^v]+v=(.{11}).*', url)
+    return result.group(1)
+
+def geturl(video):
+    a = YouTubeTranscriptApi.get_transcript(video)
+    list1 = []
+
+    for key in range(0,len(a),3):
+        try:
+            i1 = a[key].get('text')
+            i2 = a[key+1].get('text')
+            i3 = a[key+2].get('text')
+        except IndexError:
+            pass
+
+        final = i1 + i2 + i3
+        list1.append(final)
+        
+    final_str = '\n'.join(list1)
+
+    with open('file.txt' , 'w') as f:
+        f.write(str(final_str))
 
 
 def homepage(request):
     if request.method == 'POST':
         yt_video = request.POST.get('hack')
-        return redirect(f'http://127.0.0.1:8000/results/{yt_video}')
+        vid_id = id_video(yt_video)
+        return redirect(f'http://127.0.0.1:8000/results/{vid_id}')
         
     return render(request , 'Website/index.html')
 
 def results(request ,url): 
-    return HttpResponse(f'{url}')
+    geturl(url)
+    summarized = summarize1()
+    context = {'summary' : summarized.replace('\n','').title()}
+    return render(request , 'Website/result.html' , context)
