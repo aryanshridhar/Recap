84415c89a08cb584a7d4ca52b27992760a7ea416
Tarun Raj <tarun@pop-os.localdomain>
2020-03-14 17:08:20 +0530

    Added ml code

--- Recap/Website/ml.py
+++
@@ -1,9 +0,0 @@
-from gensim.summarization import summarize
-
-def summarize():
-    f = open('file.txt','r')
-    raw = f.read()
-    f.close()
-    sumz=summarize(raw,word_count=100)
-
-    return sumz
