b1baeb7def22dfcb3572809b62539e6863636066
aryanshridhar <aryanshridhar7@gmail.com>
2020-03-14 18:51:14 +0530

    Added backend code

---
+++ Recap/Website/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('' , views.homepage , name = 'homepage'),
+    path('results/<str:url>' , views.results , name = 'results'),
+]
